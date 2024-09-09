c4039db2b96a3ad03e66117dfe0449114c234019
Marco <marquino171@gmail.com>
2024-09-09 16:41:57 -0300

    Add files via upload

---
+++ ProvaPOO/ProvaPOO/NomeProprio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao1
+{
+    internal class NomeProprio
+    {
+        public string NomeCompleto { get; private set; }
+        public string NomePaper { get; private set; }
+
+        // Construtor que recebe o nome completo
+        public NomeProprio(string nomeCompleto)
+        {
+            NomeCompleto = nomeCompleto;
+            GerarNomePaper();
+        }
+
+        // Método que gera o nome no formato "Sobrenome, Primeiro Nome e Inicial do Nome do Meio"
+        private void GerarNomePaper()
+        {
+            string[] partesNome = NomeCompleto.Split(' ');
+
+            if (partesNome.Length >= 2)
+            {
+                string primeiroNome = partesNome[0];
+                string sobrenome = partesNome[partesNome.Length - 1];
+                string nomeDoMeio = partesNome.Length > 2 ? partesNome[1][0] + "." : "";
+
+                NomePaper = $"{sobrenome}, {primeiroNome} {nomeDoMeio}";
+            }
+            else
+            {
+                // Caso o nome seja composto por apenas uma parte
+                NomePaper = NomeCompleto;
+            }
+        }
+
+        // Método que imprime o nome no formato "paper"
+        public void ImprimeNomePaper()
+        {
+            Console.WriteLine(NomePaper);
+        }
+    }
+}
---
+++ ProvaPOO/ProvaPOO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao1
+{
+    static class Questao1
+    {
+        static void Main(string[] args)
+        {
+            // Solicitar o nome completo do usuário
+            Console.WriteLine("Digite o nome completo:");
+            string nomeCompleto = Console.ReadLine();
+
+            // Criar objeto da classe NomeProprio
+            NomeProprio seunome = new NomeProprio(nomeCompleto);
+
+            // Imprimir o nome no formato "paper"
+            Console.WriteLine("Nome no formato acadêmico:");
+            seunome.ImprimeNomePaper();
+
+            // Manter o console aberto até uma tecla ser pressionada
+            Console.ReadKey();
+        }
+    }
+}
